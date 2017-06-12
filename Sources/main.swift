df0a75c04842200a1234dce08f6c0ee2523bf910
white <316949206@qq.com>
2017-06-12 20:15:10 +0800

    main.swift init

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,25 @@
-print("Hello")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+
+var routes = Routes();
+routes.add(method: .get, uri: "/", handler: {
+    HTTPRequest, HTTPResponse in 
+    HTTPResponse.setHeader(.contentType, value: "text/html")
+    HTTPResponse.appendBody(string: "<html><title>hello</title><body>hello</body></html>")
+    HTTPResponse.completed()
+})
+
+server.add(route: routes)
+
+server.serverPort = 8181
+
+do {
+    try server.start()
+ } catch PerfectError.networkError(let err, let msg) {
+     print("网络错误 \(err) \(msg)")
+ }
+
+// print("Hello")
